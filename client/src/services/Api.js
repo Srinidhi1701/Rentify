70af9c7f0ac4fd38aaf37e958c854bf64dce357d
Srinidhi1701 <rsrinidhi1701@gmail.com>
2026-07-01 22:38:21 +0530

    added the backend url from render

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rentify-cms3.onrender.com",
 });
 
 // Automatically attach token to every request
